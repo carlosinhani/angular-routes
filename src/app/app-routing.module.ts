fcab296179a1e9084c438f6fb723ecec3e2306f1
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-23 19:30:12 -0300

    params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+const appRoutes: Routes = [
+      { path: 'cursos', component: CursosComponent},
+      { path: 'curso/:id', component: CursoDetalheComponent},
+      { path: 'login', component: LoginComponent},
+      { path: '', component: HomeComponent }
+];
+
+// export const routing: ModuleWithProviders = RouterModule.forRoot(AppRoutingModule);
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
