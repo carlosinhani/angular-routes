9403f865fe4d33e2cfd2dc5342667ffd3b2f14b8
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-24 15:14:35 -0300

    url_query

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+import { threadId } from 'worker_threads';
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -9,11 +13,34 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any [];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+            private cursosService: CursosService,
+            private appRoutes: ActivatedRoute,
+            private router: Router
+
+    ) { }
 
   ngOnInit(){
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.appRoutes.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    // this.pagina++;
+    this.router.navigate(['/cursos'],
+     {queryParams: {'pagina': ++this.pagina}});
   }
 
 }
